2a7934533477a7ebde3e0a1c981061f8513ebba2
Attie Grande <attie.grande@argentum-systems.co.uk>
2021-12-10 19:54:57 +0000

    soc: atmel_sam0: Add support for SAML21 parts
    Implement support for SAML21 parts, tested on a SAML21 Xplained Pro dev
    board. Confirmed operational peripherals: UART, GPIO, PWM, ADC.
    
    Signed-off-by: Attie Grande <attie.grande@argentum-systems.co.uk>

---
+++ soc/arm/atmel_sam0/common/adc_fixup_saml21.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021 Argentum Systems Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _ATMEL_SAM_ADC_FIXUP_SAML21_H_
+#define _ATMEL_SAM_ADC_FIXUP_SAML21_H_
+
+#define ADC_REFCTRL_REFSEL_VDD_1_2 ADC_REFCTRL_REFSEL_INTVCC1
+#define ADC_REFCTRL_REFSEL_VDD_1   ADC_REFCTRL_REFSEL_INTVCC2
+
+#include "adc_fixup_sam0.h"
+
+#endif /* _ATMEL_SAM_ADC_FIXUP_SAML21_H_ */
